d7363353115e0de7e789c93fd59a4e7d779f0c51
xyu <1357525676@qq.com>
2021-02-24 15:16:59 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <workflow></workflow>
+    </div>
+</template>
+
+<script>
+    import Workflow from './components/Workflow';
+
+    export default {
+        name: 'App',
+        components: {
+            Workflow
+        }
+    }
+</script>
+
+<style>
+    body {
+        margin: 0;
+    }
+</style>
